eeb83324beb2ddeaedcd0a3d8317fc02904f3022
T3hreem <threeminal@gmail.com>
2022-05-27 01:45:22 +0430

    welcome page is done.

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,9 +1,98 @@
 import 'package:flutter/material.dart';
+import 'signup.dart';
+import 'login.dart';
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 0,
+                  ),
+                  Text("                             to Reddit",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 20,
+                    ),
+                  ),
+                  Image.asset('assets/logo.png'),
+                  SizedBox(
+                    height: 90,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      //login
+                      MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                        },
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Colors.black
+                            ),
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      //sign up
+                      MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                        },
+                        color: Colors.deepOrange,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 18,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
