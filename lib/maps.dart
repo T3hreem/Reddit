646788a952d4d9ef9636681e0893696cc5c3b6e2
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-27 13:15:19 +0430

    maps page was created

---
+++ lib/maps.dart
@@ -0,0 +1,24 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class maps extends StatefulWidget {
+  const maps ({Key key}) : super(key: key);
+
+  @override
+  State<maps> createState() => _maps();
+}
+
+
+class _maps extends State<maps> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+          'Qolam2'
+      ),
+    );
+  }
+}
