811a8d0db260ff14aed8b08ec32d2f8ad0550343
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-27 13:14:06 +0430

    add page was created

---
+++ lib/add.dart
@@ -0,0 +1,24 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class add extends StatefulWidget {
+  const add ({Key key}) : super(key: key);
+
+  @override
+  State<add> createState() => _add();
+}
+
+
+class _add extends State<add> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+          'Qolam3'
+      ),
+    );
+  }
+}
