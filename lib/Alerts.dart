118c18d0c058a0d98fb0d8cbfc2120b7290fbcf7
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-27 13:14:28 +0430

    Alerts page was created

---
+++ lib/Alerts.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Alerts extends StatefulWidget {
+  const Alerts ({Key key}) : super(key: key);
+
+  @override
+  State<Alerts> createState() => _Alerts();
+}
+
+
+class _Alerts extends State<Alerts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'Qolam'
+      ),
+    );
+  }
+}
