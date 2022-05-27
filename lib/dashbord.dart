fd3c07f58b6671b623aed02fd7a28673033f552a
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-27 13:14:55 +0430

    dashboard page was created

---
+++ lib/dashbord.dart
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class dashbord extends StatefulWidget {
+  const dashbord ({Key key}) : super(key: key);
+
+  @override
+  State<dashbord> createState() => _dashbord();
+}
+
+
+class _dashbord extends State<dashbord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+          'Qolam4'
+      ),
+    );
+  }
+}
