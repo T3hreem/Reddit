8be61a7d2fd38a355c4ad2ad3cc97559ff7dc6b4
S-Abolfazl <100437667+S-Abolfazl@users.noreply.github.com>
2022-05-29 01:40:52 +0430

    complete the posts and create class ...

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,4 +1,5 @@
 
+// import 'dart:ffi';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +14,40 @@ void main() {
   runApp(const MyApp());
 }
 
+class User{
+  Community community;
+  String name;
+  User(this.name,this.community);
+}
+
+class Community{
+  //User users;
+  String name;
+  Community(this.name);
+// Array user;
+}
+
+class Post{
+  int comment;
+  int vote;
+  Image profile;
+  User user;
+  String post;
+
+  Post(int comment,int vote,User user,String post){
+    this.comment = comment;
+    this.vote = vote;
+    this.post = post;
+    this.user = user;
+  }
+
+}
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,8 +63,6 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-  //MaterialColor Color = Colors.red;
-  //TextStyle textStyle = textStyle.merge(Alignment.center);
   @override
   State<MyHomePage> createState() => _MyHomePageState(0);
 
@@ -41,22 +70,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Post> list = [
+    Post(55, 33, User("maaamod",Community("company1")), "helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"),
+    Post(52, 655, User("nader",Community("company2")), "salam"),
+    Post(500, 33, User("abolfazl",Community("company3")), "bye"),
+    Post(53, 6, User("mehdi",Community("company4")), "bah bah"),
+    Post(52, 9, User("morteza",Community("company5")), "manteghie"),
+    Post(55, 8, User("mehdi m",Community("company6")), "eeeeeee"),
+    Post(52, 22, User("eshgam j",Community("company2")), "feshar bokhor"),
+    Post(50, 22121, User("mehdi zi",Community("company1")), "eeeeee"),
+  ];
 
   int SelectedPage;
+  int listCounter;
   int index;
   final double iconSize = 30;
 
+
   _MyHomePageState(this.SelectedPage);
   List<Widget> pages = [Alerts(),maps(),add(),dashbord()];
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      //appBar: AppBar(title: Text(widget.title,style:TextStyle(color: Colors.purple,fontSize: 25,fontWeight: FontWeight.bold)),),
+
       title:"Reddit",
 
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        // fontFamily: 'Parisienne',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
@@ -65,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //leading: Icon(Icons.pending,color: Colors.indigo,),
           leading: IconButton(
             icon: Icon(
-              Icons.pending,
+              Icons.dehaze,
               color: Colors.indigo,
             ),
             padding: EdgeInsets.only(left: 10),
@@ -116,92 +158,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
 
-
-        body: Container(
-
-          child: Column(
-            children:[
-
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(left: 10,top: 10),
-                    child : CircleAvatar(
-                      child: Image(
-                        image: AssetImage("assets/icon.jpg"),
-                        fit: BoxFit.cover,),
-                      backgroundColor: Colors.lightGreen,
-                      radius: 20,
-                    ),
-                  ),
-                  const SizedBox(width:10,),
-                  Column(
-                    children: [
-                      Text(
-                        'userrname',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontFamily: "fonts/DancingScript-Regular.ttf",
-                          height: 1.5,
-                        ),
-                      ),
-
-                      //const SizedBox(height: 10,),
-                      Text(
-                        'Comment!........',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: Colors.grey[600],
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal,
-                          //textBaseline: TextBaseline.ideographic,
-                          fontFamily:'assets/Merriweather-BoldItalic.ttf',
-                          height: 1.2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-                //margin:EdgeInsets.only(right: 250,left:10,bottom:470),
-
-              ),
-
-              Card(
-                  color: Colors.grey[200],
-                  child: Padding(
-                    padding: EdgeInsets.only(right:10,left: 10,top: 10),
-                    child: TextField(
-                      readOnly: true,
-                      maxLines: 5, //or null
-                      onTap: null,
-                      decoration: InputDecoration.collapsed(
-                          hintText: "What does we should to leaarn to be a nice programmer!"),
-                    ),
-                  )
-              ),
-
-            ],
-          ),
-
-          //alignment: Alignment.center,
-          // decoration: BoxDecoration(
-          //   gradient: LinearGradient(
-          //     begin: Alignment.topLeft,
-          //     end: Alignment.bottomRight,
-          //     colors: [
-          //       Color(0xFFffadad),
-          //       Color(0xFFffd6a5),
-          //       Color(0xFFfdffb6),
-          //       Color(0xFFcaffbf),
-          //       Color(0xFF9bf6ff),
-          //       Color(0xFFa0c4ff),
-          //       Color(0xFFbdb2ff),
-          //       Color(0xFFffc6ff),
-          //       Color(0xFFfffffc),
-          //
-          //     ],
-          //   ),
-          // ),
+        body:ListView.builder(itemCount: 5,
+            itemBuilder: (BuildContext context,int index){
+              return _widget(list[index]);
+            }
         ),
 
         bottomNavigationBar: BottomNavigationBar(
@@ -246,9 +206,7 @@ class _MyHomePageState extends State<MyHomePage> {
               label: '',
             )
           ],
-          onTap:
-          _onItemTapped,
-
+          onTap:_onItemTapped,
         ),
       ),
     );
@@ -266,5 +224,249 @@ class _MyHomePageState extends State<MyHomePage> {
 
     });
   }
+  //alignment: Alignment.center,
+  // decoration: BoxDecoration(
+  //   gradient: LinearGradient(
+  //     begin: Alignment.topLeft,
+  //     end: Alignment.bottomRight,
+  //     colors: [
+  //       Color(0xFFffadad),
+  //       Color(0xFFffd6a5),
+  //       Color(0xFFfdffb6),
+  //       Color(0xFFcaffbf),
+  //       Color(0xFF9bf6ff),
+  //       Color(0xFFa0c4ff),
+  //       Color(0xFFbdb2ff),
+  //       Color(0xFFffc6ff),
+  //       Color(0xFFfffffc),
+  //
+  //     ],
+  //   ),
+  // ),
+  _widget(Post post){
+    return Container(
+
+      child: Column(
+
+        children: [
+
+          ListTile(
+            leading: CircleAvatar(
+              child: Image(
+                image: AssetImage("assets/icon.jpg"), //aks ekhtesasi
+                fit: BoxFit.cover,),
+              backgroundColor: Colors.lightGreen,
+              radius: 20,
+            ),
+            title: Text(post.user.community.name),
+            subtitle: Text(post.user.name),
+            tileColor: Colors.black38,
+          ),
+
+          ListTile(
+            tileColor: Colors.black12,
+            title: Text(post.post),
+          ),
+
+          Row(
+            children: [
+              Container(
+                child : IconButton(
+                  icon : Icon(
+                      Icons.arrow_upward_rounded,
+                      color: Colors.black38,
+                      size: 25.0),
+                  onPressed:(){
+                    setState(() {
+                      /* on pressed    */
+                    });
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top:2),
+                child:Text(post.vote.toString(),
+                  style: TextStyle(
+                      fontSize: 15
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 2),
+                child : IconButton(
+                    onPressed: (){
+
+                    },
+                    icon : Icon(
+                      Icons.arrow_downward_rounded,
+                      color: Colors.black38,
+                      size: 25.0,
+                    )),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left:15,top: 5),
+                  child : IconButton(
+                    icon: Icon(
+                      Icons.messenger_outline_outlined,
+                      color: Colors.black38,
+                      size: 25.0,
+                    ),
+                    onPressed: (){
+
+                    },
+                  )
+
+              ),
+              Container(
+                margin: EdgeInsets.only(top:1),
+                child:Text(post.comment.toString(),
+                  style: TextStyle(
+                      fontSize: 15
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 18,top: 5),
+                  child : IconButton(
+                    icon: Icon(
+                      Icons.share,
+                      color: Colors.black38,
+                      size: 25.0,
+                    ),
+                    onPressed: (){
+
+                    },
+                  )
+
+              ),
+              Container(
+                margin: EdgeInsets.only(top:5,left:5),
+                child:Text(
+                  'Share',
+                  style: TextStyle(
+                      fontSize: 15
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 25,top: 5),
+                  child : IconButton(
+                    icon:Icon(
+                      Icons.card_giftcard,
+                      color: Colors.black38,
+                      size: 25.0,
+                    ),
+                    onPressed: (){
+
+                    },
+                  )
+
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+
+
 }
+/*
+Row(
+    children: [
+    Container(
+    child : IconButton(
+    icon : Icon(
+    Icons.arrow_upward_rounded,
+    color: Colors.black38,
+    size: 25.0),
+    onPressed:(){
+    setState(() {
+    /* on pressed    */
+    });
+    },
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.only(top:2),
+    child:Text(list[listCounter].vote.toString(),
+    style: TextStyle(
+    fontSize: 12
+    ),
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.only(top: 2),
+    child : IconButton(
+    onPressed: (){
+
+    },
+    icon : Icon(
+    Icons.arrow_downward_rounded,
+    color: Colors.black38,
+    size: 25.0,
+    )),
+    ),
+    Container(
+    margin: EdgeInsets.only(left:15,top: 5),
+    child : IconButton(
+    icon: Icon(
+    Icons.messenger_outline_outlined,
+    color: Colors.black38,
+    size: 25.0,
+    ),
+    onPressed: (){
+
+    },
+    )
+
+    ),
+    Container(
+    margin: EdgeInsets.only(top:1),
+    child:Text("iuhjiygyuhn",
+    style: TextStyle(
+    fontSize: 12
+    ),
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.only(left: 18,top: 5),
+    child : IconButton(
+    icon: Icon(
+    Icons.share,
+    color: Colors.black38,
+    size: 25.0,
+    ),
+    onPressed: (){
+
+    },
+    )
+
+    ),
+    Container(
+    margin: EdgeInsets.only(top:5,left:5),
+    child:Text(
+    'Share',
+    style: TextStyle(
+    fontSize: 14
+    ),
+    ),
+    ),
+    Container(
+    margin: EdgeInsets.only(left: 25,top: 5),
+    child : IconButton(
+    icon:Icon(
+    Icons.card_giftcard,
+    color: Colors.black38,
+    size: 25.0,
+    ),
+    onPressed: (){
+
+    },
+    )
+
+    ),
+    ],
+    )*/
 
