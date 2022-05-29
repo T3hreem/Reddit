bbe4349a2b5e9f71881dd7748f8425972313a751
T3hreem <threeminal@gmail.com>
2022-05-30 02:31:07 +0430

    chat page is done.

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,8 +1,8 @@
 import 'package:ap_project/dashbord.dart';
+import 'package:ap_project/premium.dart';
 import 'package:flutter/material.dart';
 import 'dashbord.dart';
 import 'Alerts.dart';
-import 'add.dart';
 import 'feed.dart';
 import 'maps.dart';
 
@@ -18,7 +18,55 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Chats",style: TextStyle
+          (color: Colors.black),),
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            SizedBox(height: 100,),
+            Image.asset('assets/chat.png'),
+            SizedBox(height: 10,),
+            MaterialButton(
+              minWidth: 250,
+              height: 60,
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => PremiumPage()));
+              },
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                    color: Colors.black
+                ),
+                  borderRadius: BorderRadius.circular(50)
+              ),
+              child: Text(
+                "Start chatting",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: 30,
@@ -27,7 +75,7 @@ class _ChatPageState extends State<ChatPage> {
             icon: Icon(
               SelectedPage==0?Icons.home:
               Icons.home_outlined,
-              color: Colors.deepOrange,
+              color: Colors.black,
 
             ),
             label: 'Home',
@@ -36,28 +84,28 @@ class _ChatPageState extends State<ChatPage> {
           BottomNavigationBarItem(
             icon: Icon(
               SelectedPage==1? Icons.dashboard:Icons.dashboard_outlined,
-              color: Colors.deepOrange,
+              color: Colors.black,
             ),
             label: 'Discover',
           ),
           BottomNavigationBarItem(
             icon: Icon(SelectedPage==2?Icons.add_circle:
             Icons.add,
-              color: Colors.deepOrange,
+              color: Colors.black,
             ),
             label: 'Creat',
           ),
           BottomNavigationBarItem(
             icon: Icon(SelectedPage==3?Icons.chat_rounded:
             Icons.chat_outlined,
-              color: Colors.deepOrange,
+              color: Colors.black,
             ),
             label: 'Chat',
           ),
           BottomNavigationBarItem(
             icon: Icon(SelectedPage==4?Icons.add_alert:
             Icons.add_alert_outlined,
-              color: Colors.deepOrange,
+              color: Colors.black,
             ),
             label: 'Inbox',
           )
